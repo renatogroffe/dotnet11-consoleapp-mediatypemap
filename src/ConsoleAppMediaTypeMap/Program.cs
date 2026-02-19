321c813f1d5e26333f56612b7e3d7955f3fc06f3
Renato Groffe <renato.groffe@yahoo.com.br>
2026-02-18 23:01:42 -0300

    Commit inicial

---
+++ src/ConsoleAppMediaTypeMap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mime;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com .NET 11 | O tipo System.Net.Mime.MediaTypeMap *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+Console.WriteLine();
+
+Console.WriteLine($"MediaTypeMap.GetMediaType(\".mp3\") = {MediaTypeMap.GetMediaType(".mp3")}");
+Console.WriteLine($"MediaTypeMap.GetMediaType(\"logo.svg\") = {MediaTypeMap.GetMediaType("logo.svg")}");
+Console.WriteLine($"MediaTypeMap.GetExtension(\"text/plain\") = {MediaTypeMap.GetExtension("text/plain")}");
+
+ReadOnlySpan<char> path = "/c/temp/document.docx";
+Console.WriteLine($"MediaTypeMap.GetMediaType(ReadOnlySpan<char>) = {MediaTypeMap.GetMediaType(path)}");
